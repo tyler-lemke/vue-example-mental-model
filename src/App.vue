b02ffae90ab90e1b5bbaa7eee540a18129f4f9a5
tylerslemke <tyleris@gmail.com>
2022-07-16 12:57:30 -0700

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-<!-- 3. finally needs to be defined here -->
-	<Counter />  
+  <!-- 3. finally needs to be defined here -->
+  <Counter />
 </template>
 
 <script>
 //imports always at the top of the file
-// import Counter from './components/Counter.vue' //1. using components needs import
+import Counter from './components/Counter.vue'; //1. using components needs import
 
 export default {
   name: 'App',
   components: {
     //  Counter // 2. declare import inside the components object
-  }
-}
+  },
+};
 </script>
